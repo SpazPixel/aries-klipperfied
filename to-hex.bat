0997b7a83fe5f8fe8779b4ca6641a77e3049af14
SpazPixel <65928357+SpazPixel@users.noreply.github.com>
2026-06-15 22:32:30 +1000

    Update to-hex.bat

--- to-hex.bat
+++ to-hex.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\srecord\bin\srec_cat.exe" "./klipper.bin" -binary -offset 0x08000000 -output "./firmware.hex" -Intel
+"C:\Program Files\srecord\bin\srec_cat.exe" "[USB Root Dir]/klipper.bin" -binary -offset 0x08000000 -output "[USB Root Dir]/firmware.hex" -Intel
 pause
